00dae4cfeeb87b6fe0960ae31bbde4b070851720
Brian Sheaffer <brsh@users.noreply.github.com>
2019-03-04 12:45:15 -0800

    Added DriveType to report

--- private/Format-DriveInfo.ps1
+++ private/Format-DriveInfo.ps1
@@ -32,21 +32,23 @@
 		[string] $BlockSize,
 		[Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true)]
 		[string] $PageFile,
+		[Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true)]
+		[string] $DriveType,
 		[Parameter(Mandatory = $false)]
 		[switch] $BGInfo = $false
 	)
 
 	BEGIN {
 		[int] $i = 0
-		$columns = "{0,-2}   {1,4} {2,9} {3,9}  {4,-7} {5,9}  {6,-8}  {7}"
-		Write-Host "Drive Info:`t$($columns -f '', 'Free', 'Free', 'Total', 'Format', 'BlockSize', 'PageFile', 'Label')"
+		$columns = "{0,-2}   {1,-10}  {2,4} {3,9} {4,9}  {5,-7} {6,9}  {7,-8}  {8}"
+		Write-Host "Drive Info:`t$($columns -f '', 'Type', 'Free', 'Free', 'Total', 'Format', 'BlockSize', 'PageFile', 'Label')"
 		[string] $tab = "`t`t"
 		if ($BGInfo) { $tab = "`t"}
 	}
 
 	PROCESS {
 		[string] $Build = ""
-		$Build = $tab + $columns -f $Drive, "$("{0:N0}" -f $FreePercent)%", "$("{0:N0}" -f $FreeSpace)GB", "$(("{0:N0}" -f $Size))GB", $FileSystem, $BlockSize, $PageFile, $VolumeName
+		$Build = $tab + $columns -f $Drive, $DriveType, "$("{0:N0}" -f $FreePercent)%", "$("{0:N0}" -f $FreeSpace)GB", "$(("{0:N0}" -f $Size))GB", $FileSystem, $BlockSize, $PageFile, $VolumeName
 		Write-Host $Build
 	}
 
